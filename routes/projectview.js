92af5fbaae2fdb974fd45a024f8f62fa6de69ce0
Rohin Adalja <contact@rohinadalja.com>
2019-03-04 15:58:29 -0500

    Updates to Export PDF function

--- routes/projectview.js
+++ routes/projectview.js
@@ -96,6 +96,39 @@ router.get('/', isLoggedIn, api.getDocumentStatusSite, function(req, res, next) 
          });
 
 
+		router
+				 .get('/exportPDF/:id', isLoggedIn, api.getDocumentSite, api.getProjPartnersLeaders,
+					 api.getAssignableUsers, api.getWrapUpDoc, api.getProjectPlanDoc,
+					 api.getLeadtimeDefaults,
+					 function(req, res, next) {
+					 //Checking what's in params
+					 //console.log("Rendering application " + ObjectId(req.params.id));
+				 
+						 console.log("Export to PDF Called");
+					 var payload = {};
+				 
+						 payload.doc = res.locals.results.doc[0];
+						 payload.work = res.locals.results.work;
+						 payload.user = req.user._id;
+						 payload.user_email = res.locals.email;
+							 payload.user_role = res.locals.role;
+							 payload.user_roles = res.locals.user_roles;
+						 payload.projectNotes = res.locals.results.projectNotes;
+					 payload.assignableUsers = res.locals.assignableUsers;
+					 payload.wrapUp = res.locals.wrapUp ? res.locals.wrapUp : ProjectWrapUpPackage.empty(req.params.id);
+						 payload.part = res.locals.results.part||req.partnerTime;			//Data for Partners Tab Partial
+					 payload.plan = res.locals.plan || ProjectPlanPackage.empty(req.params.id)
+					 payload.leadtime = res.locals.leadtime;
+						 payload.partDocId = res.locals.results.doc[0]._id;
+						 console.log("results - export as PDF");
+					 console.log(payload);
+					 
+						 // res.render('siteassessmenttool', payload);
+						 res.render('exportPDF', payload);
+				 
+			});
+
+
 //same as vetting route.  Shouldn't be issues with logic as is
 router.route('/additem')
 	.post(isLoggedInPost, api.addWorkItem, function(req, res) {
@@ -128,7 +161,7 @@ router.route('/updatesummary')
         res.json(res.locals);
     }
 	});	
-
+	
 router.route('/wrapup')
       .post(isLoggedInPost, api.saveProjectWrapUp, function(req, res) {
 	      if(res.locals.status != '200'){
