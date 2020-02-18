e47285917e5566ae15df29e96cc439f01858c074
Rohin Adalja <rohin.adalja@mail.utoronto.ca>
2020-02-18 18:51:36 -0500

    Fix backup script
    Fix automated backup script crontab

--- script/init-crontab.sh
+++ script/init-crontab.sh
@@ -5,6 +5,6 @@ set -e
 [ -d /usr/src/logs ] || mkdir /usr/src/logs/
 touch /usr/src/logs/cron_s3.log
 
-command="/usr/src/Catalyst-AppVetting/script/db-backup.sh >> /usr/src/logs/cron_s3.log 2>&1"
+command="/usr/src/Catalyst-AppVetting/script/db-backup.sh -yes >> /usr/src/logs/cron_s3.log 2>&1"
 job="00 23 * * * $command"
 cat <(fgrep -i -v "$command" <(crontab -l)) <(echo "$job") | crontab -
