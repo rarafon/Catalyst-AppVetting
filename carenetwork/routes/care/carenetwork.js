d7c10c371a7f6719a0dc529e9c589f9e3137751d
Andy Situ <and.situ@gmail.com>
2020-04-18 16:39:17 -0700

    Create function to set context in helper.js w/ promise

--- carenetwork/routes/care/carenetwork.js
+++ carenetwork/routes/care/carenetwork.js
@@ -5,13 +5,19 @@ var router = express.Router();
 var helper = require("../../service/helper");
 
 router.get('/', helper.isLoggedIn, function(req, res) {
-  var context = {"carenetwork": true};
-  res.render("care/index", context);
+  helper.create_user_context(req).then(
+    (context) => {
+      res.render("care/index", context);
+    }
+  );
 });
 
 router.get('/application', helper.isLoggedIn, function(req, res){
-  var context = {"carenetwork": true};
-  res.render("care/application", context);
+  helper.create_user_context(req).then(
+    (context) => {
+      res.render("care/application", context);
+    }
+  );
 });
 
 module.exports = router;
--- carenetwork/service/helper.js
+++ carenetwork/service/helper.js
@@ -1,4 +1,30 @@
+/***
+ * Helper Functions for Care Network
+ */
+
 var express = require('express');
+// var db = require('../mongoose/connection');
+var User = require('../../models/userPackage');
+
+
+// Uses Promises to retrieve user info. Returns context object
+function create_user_context(req) {
+  var userID = req.user._id.toString();
+
+  var myPromise = new Promise((resolve, reject) => {
+      var result = User.findOne({"_id": userID}).lean();
+      resolve(result);
+    });
+
+  return myPromise.then( (result) => {
+    return {
+      // user_role: result.user.user_role,
+      user_email: result.contact_info.user_email,
+      user: true,
+      carenetwork: true
+    }
+  });
+}
 
 //check to see if user is logged in and a vetting agent or site or an admin
 function isLoggedIn(req, res, next) {
@@ -11,5 +37,5 @@ function isLoggedIn(req, res, next) {
   }
 }
 
-
 module.exports.isLoggedIn = isLoggedIn;
+module.exports.create_user_context = create_user_context;
