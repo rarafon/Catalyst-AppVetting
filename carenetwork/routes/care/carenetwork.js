e0b940652bd48128c84e3b0cc080d40a3acc2b32
Andy Situ <and.situ@gmail.com>
2020-04-19 23:40:05 -0700

    Submitting application works
    Needs cleaning up & refactoring, but works

--- carenetwork/routes/care/carenetwork.js
+++ carenetwork/routes/care/carenetwork.js
@@ -4,15 +4,38 @@ var router = express.Router();
 
 var helper = require("../../service/helper");
 
-router.get('/', helper.isLoggedIn, function(req, res) {
+var CareApplicant = require('../../models/care/careApplicant');
+// var CareContact = require('../../models/care/careContact');
+
+var application_service = require('../../service/care/application.js');
+
+// router.get('/', helper.isLoggedIn, function(req, res) {
+  router.get('/', function(req, res) {
   helper.create_user_context(req).then(
     (context) => {
       res.render("care/index", context);
     }
+  );  
+});
+
+router.get('/application', function(req, res){
+  helper.create_user_context(req).then(
+    (context) => {
+      res.render("care/application", context);
+    }
   );
 });
 
-router.get('/application', helper.isLoggedIn, function(req, res){
+
+router.post('/application', async function(req, res) {
+  if (application_service.check_care_application(req.body)) {
+      await application_service.create_care_applicant(req.body)
+      res.status(200).end(); // OK creation
+  } else
+    res.status(404).end(); // Missing fields
+});
+
+router.get('/view_apps', function(req, res){
   helper.create_user_context(req).then(
     (context) => {
       res.render("care/application", context);
---
+++ carenetwork/service/care/application.js
@@ -0,0 +1,182 @@
+
+// var mongoose = require('mongoose');
+// var db = require('../../../mongoose/connection');
+var CareApplicant = require('../../models/care/careApplicant');
+var CareContact = require('../../models/care/careContact');
+
+// Check that all required data was provided using fields obj
+// into body of request
+function check_care_application(req_body) {
+  console.log(req_body);
+  for (field in fields_map) {
+    if (fields_map[field]["required"]) {
+      if (req_body[field] == undefined || req_body[field].length <= 0){
+        console.log("missing");
+        console.log(field)
+        return false;
+      }
+        
+    }
+  }
+  return true;
+}
+
+async function create_care_applicant(req_body) {
+  var field;
+
+  var careApplicant = new CareApplicant(),
+      careContact = new CareContact();
+
+  var path_arr, i, value, field_obj;
+
+  for (field in fields_map) {
+    field_obj = fields_map[field];
+    
+    value = req_body[field];
+
+    if (value == undefined || value.length <= 0)
+      continue;
+
+    // Split path string into array. Used for navigation
+    path_arr = field_obj.path.split("/");
+
+    var o;
+    if (field_obj.schema == "careApplicant")
+      o = careApplicant;
+    else
+      o = careContact
+    
+    // Navigate through model objects thru path array
+    for (i=0; i< path_arr.length; i++) {
+      if (i == path_arr.length - 1)
+        o[path_arr[i]] = value;
+      else
+        o = o[path_arr[i]]
+    }
+  }
+  // Add IDs of each other
+  careApplicant.application.contacts.push(careContact._id);
+  careContact.applicant_id = CareApplicant._id;
+
+  await careContact.save()
+  await careApplicant.save();
+  return true
+}
+
+// Map for names of the HTML inputs, required for db, schema name, & path for schema variable
+var fields_map = {
+  first_name: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/first_name"
+  },
+  middle_name: {
+    required: false,
+    schema: "careApplicant",
+    path: "application/middle_name"
+  },
+  last_name: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/last_name"
+  },
+  email: {
+    required: false,
+    schema: "careApplicant",
+    path: "application/email"
+  },
+  phone: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/phone"
+  },
+  dob: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/dob"
+  },
+  marital_status: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/marital_status"
+  },
+  line_1: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/address/line_1"
+  },
+  line_2: {
+    required: false,
+    schema: "careApplicant",
+    path: "application/address/line_2"
+  },
+  city: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/address/city"
+  },
+  state: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/address/state"
+  },
+  zip: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/address/zip"
+  },
+  health_issues: {
+    required: false,
+    schema: "careApplicant",
+    path: "application/health_issues"
+  },
+  help_request: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/help_request"
+  },
+  home_occupants: {
+    required: false,
+    schema: "careApplicant",
+    path: "application/home_occupants"
+  },
+  refer_text: {
+    required: false,
+    schema: "careApplicant",
+    path: "application/refer_text"
+  },
+  signature: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/signature"
+  },
+  signature_date: {
+    required: true,
+    schema: "careApplicant",
+    path: "application/signature_date"
+  },
+  // Contact Schema
+  contact_name: {
+    required: true,
+    schema: "careContact",
+    path: "name"
+  },
+  contact_relationship: {
+    required: true,
+    schema: "careContact",
+    path: "relationship"
+  },
+  contact_phone: {
+    required: true,
+    schema: "careContact",
+    path: "phone"
+  },
+  contact_email: {
+    required: false,
+    schema: "careContact",
+    path: "email"
+  },
+};
+
+module.exports.check_care_application = check_care_application;
+module.exports.create_care_applicant = create_care_applicant;
